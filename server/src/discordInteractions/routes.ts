f69782071e8e600584af07802a17d5d3f83cd6c9
Ryan Rolnicki <git@rolnicki.net>
2023-04-05 07:12:04 +0900

    interaction handler

--- server/src/discordInteractions/routes.ts
+++ server/src/discordInteractions/routes.ts
@@ -12,7 +12,7 @@ export default function discordInteractionsHandler(
     reply.code(200).send('get')
   })
   server.post('', async (req, res) => {
-    const body = req.body
+    const body = req.body as any
     const sig = req.headers['X-Signature-Ed25519'] as string
     const ts = req.headers['X-Signature-Timestamp'] as string
     const appPublicKey =
@@ -22,6 +22,38 @@ export default function discordInteractionsHandler(
     console.log(req.rawBody)
 
     const isValidRequest = verifyKey(req.rawBody, sig, ts, appPublicKey)
+    console.log('Check verify')
+    if (!isValidRequest) {
+      console.log('Unverified')
+      res.status(401).send('invalid request signature')
+      return
+    }
+
+    console.log('Check body 1')
+
+    // Replying to ping (requirement 2.)
+    if (body.type == InteractionType.PING) {
+      console.log('Body1')
+      res.status(200).send({ type: InteractionType.PING })
+      return
+    }
+
+    console.log('Check foo')
+    // Handle /foo Command
+    if (body.data.name == 'foo') {
+      console.log('Body')
+      console.log(body)
+      console.log('Body data')
+      console.log(body.data)
+      res.status(200).send({
+        type: 4,
+        data: { content: 'bar' },
+      })
+      return
+    }
+
+    console.log('404')
+    res.status(404).send('unknown command')
   })
 
   done()
