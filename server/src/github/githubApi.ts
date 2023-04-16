8e0605d3b7837d5b3dd77c7cb71f9158a9b5f3f0
Jeffery Patterson <jpatterson933@ucla.edu>
2023-04-15 18:46:05 -0700

    1. Setting up test routes in githubAPI.ts

--- server/src/github/githubApi.ts
+++ server/src/github/githubApi.ts
@@ -1,9 +1,53 @@
+import { Octokit } from "@octokit/rest";
+// const Octokit = require("octokit");
+
+const octokit = new Octokit({
+  auth: process.env.GITHUB_APP_SECRET,
+});
+
+const orgName = "Hack-Weekly";
+octokit.repos
+  .listForOrg({
+    org: orgName,
+  })
+  .then(({ data }) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
+
+/**
+
+import { Octokit } from "@octokit/rest";
+
+const octokit = new Octokit({
+  auth: process.env.GITHUB_APP_SECRET,
+});
+
+const orgName = "your-org-name";
+const teamName = "new-team-name";
+
+const teamResponse = await octokit.teams.create({
+  org: orgName,
+  name: teamName,
+  privacy: "closed", // Use "closed" for a private team or "secret" for a hidden team
+});
+
+console.log(`Team created! Team ID: ${teamResponse.data.id}`);
+
+ */
+
+
+
 // High level functions for interacting with the github API
 class GithubApi {
   async createRepoForTeam(repoName: string, teamId: string) {
     // TODO
   }
   async createTeam() {
+
     // TODO
   }
   async addUserToTeam(userId: string, teamId: string) {
