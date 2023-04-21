7bfdcba2eb710c141b2b910f55d1e31069948804
zwwuu <98434813+zwwuu@users.noreply.github.com>
2023-04-20 21:57:03 -0400

    add button transition

--- client/src/components/Button/Button.tsx
+++ client/src/components/Button/Button.tsx
@@ -1,16 +1,16 @@
-import { FC, ReactNode } from "react";
+import {ButtonHTMLAttributes, FC, ReactNode} from "react";
 import styles from "./Button.module.css";
 
-interface ButtonProps {
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   disabled?: boolean;
   className?: string;
   onClick?: () => void;
   children: ReactNode;
 }
 
-const Button: FC<ButtonProps> = ({ children, className, onClick }) => {
+const Button: FC<ButtonProps> = ({ children, className, onClick,...props }) => {
   return (
-    <button className={`${styles.button} ${className}`} onClick={onClick}>
+    <button className={`${styles.button} ${className}`} onClick={onClick} {...props}>
       {children}
     </button>
   );
