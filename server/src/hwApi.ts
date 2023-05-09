ad94fa82b545d0b108d3808c22340bbf929380a1
Ryan Rolnicki <git@rolnicki.net>
2023-05-10 01:01:14 +0900

    Pre pre purge

--- server/src/hwApi.ts
+++ server/src/hwApi.ts
@@ -36,6 +36,18 @@ export class HWApi {
       ...(githubId ? { githubId } : {}),
     })
     if (existingUser) {
+      if (githubId && !existingUser.githubId) {
+        // TODO: I don't love that people just specify an ID with no validation
+        existingUser.githubId = githubId
+        await firebaseApi.updateUser(existingUser)
+        if (existingUser.team) {
+          const curTeam = await firebaseApi.getTeam(existingUser.team)
+          await githubApi.addUserToTeam(githubId, curTeam.githubTeam)
+        }
+
+        return { message: 'Updated existing github ID' }
+      }
+
       return { error: 'User already exists' }
     }
     // Derive team from discord
@@ -195,23 +207,25 @@ export class HWApi {
       })
     await Promise.all(tasks)
     const activeTeams = await firebaseApi.getActiveTeams()
-
-    // const messageTasks = activeTeams.map((t) =>
-    //   discordApi.messageChannel(t.defaultDiscordChannel, message)
-    // )
-    // await Promise.all(messageTasks)
-    const testTeam = (await firebaseApi.getTeams()).find(
-      (t) => t.name === 'Vermillion Llama'
-    )
-    const message = `Congrats on the previous project completion, <@&${testTeam.discordRole}>! As we prepare for the next project, 
+    const message = (
+      team: TeamT
+    ) => `Congrats on the previous project completion, <@&${team.discordRole}>! As we prepare for the next project, 
 we want to make sure teams have a good idea of how many people are able to contribute. 
 If you want to continue on with your current team, please run \`/continue <your-github-id>\`,
 substituting the above with your actual github id.\n
 If you are not able to participate for the next project, you don't need to do anything, though you will be 
 removed from your team and need to be re-added if you wish to participate in future projects.
     `
-    discordApi.messageChannel(testTeam.defaultDiscordChannel, message)
+    // const messageTasks = activeTeams.map((t) =>
+    //   discordApi.messageChannel(t.defaultDiscordChannel, message(t))
+    // )
+    // await Promise.all(messageTasks)
 
+    // const testTeam = (await firebaseApi.getTeams()).find(
+    //   (t) => t.name === 'Vermillion Llama'
+    // )
+    // discordApi.messageChannel(testTeam.defaultDiscordChannel, message(testTeam))
+    return { message: activeTeams.map((t) => t.name).join(',') }
     return { message: 'Pre-purge completed' }
   }
   async purge() {
@@ -222,8 +236,8 @@ removed from your team and need to be re-added if you wish to participate in fut
     const usersToRemove = users.filter(
       (u) => u.team && u.continueStatus === 'pending'
     )
-    const tasks = usersToRemove.map((u) => this.removeUserFromTeam(u))
-    await Promise.all(tasks)
+    // const tasks = usersToRemove.map((u) => this.removeUserFromTeam(u))
+    // await Promise.all(tasks)
 
     return { message: `Removed ${usersToRemove.length} users from teams` }
   }
